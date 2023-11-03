c2655cd2d81f3e6e2ff00b17e889f57de3732e5e
takuma I <baru.poke.15@gmail.com>
2023-11-04 00:47:03 +0900

    Step01完了

--- src/JuReto.jl
+++ src/JuReto.jl
@@ -1,5 +1,7 @@
 module JuReto
 
 # Write your package code here.
-
+include("step01.jl")
+using .Step01
+export Variable
 end
---
+++ src/step01.jl
@@ -0,0 +1,10 @@
+module Step01
+
+export Variable
+
+struct Variable{T}
+    data::T
+end
+
+end
+
