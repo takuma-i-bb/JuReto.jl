7c1465bc978747de6a0a4dbceb0c9efdd163f86f
takuma I <baru.poke.15@gmail.com>
2023-11-04 08:23:34 +0900

    Step07完了

--- src/JuReto.jl
+++ src/JuReto.jl
@@ -2,8 +2,8 @@ module JuReto
 
 # Write your package code here.
 export Variable, Square, Exp, numerical_diff, backward
-include("step06.jl")
-using .Step06
+include("step07.jl")
+using .Step07
 
 include("step04.jl")
 using .Step04
---
+++ src/step07.jl
@@ -0,0 +1,84 @@
+module Step07
+abstract type MyFunction end
+using JuReto
+
+export Variable, Square, Exp, backward
+
+mutable struct Variable{T}
+    data::T
+    grad::Union{T, Nothing}
+    creator::Union{MyFunction, Nothing}
+end
+
+function Variable(data)
+    Variable(data, nothing, nothing)
+end
+
+function set_creator(val::Variable, func::MyFunction)
+    val.creator = func
+end
+
+function backward(val::Variable)
+    f = val.creator
+    if f !== nothing
+        x = f.data.input
+        x.grad = backward(f, val.grad)
+        backward(x)
+    end
+end
+
+mutable struct FunctionData
+    input::Union{Variable, Nothing}
+    output::Union{Variable, Nothing}
+end
+
+function FunctionData()
+    FunctionData(nothing, nothing)
+end
+
+struct Square <: MyFunction
+    data::FunctionData
+end
+
+function Square()
+    Square(FunctionData())
+end
+
+function forward(f::Square, x)
+    x^2
+end
+
+function backward(f::Square, dy)
+    x = f.data.input.data
+    2x*dy
+end
+
+struct Exp <: MyFunction
+    data::FunctionData
+end
+
+function Exp()
+    Exp(FunctionData())
+end
+
+function forward(f::Exp, x)
+    exp(x)
+end
+
+function backward(f::Exp, dy)
+    x = f.data.input.data
+    exp(x)*dy
+end
+
+function (f::MyFunction)(input::Variable)
+    x = input.data
+    y = forward(f,x)
+    output = Variable(y)
+    set_creator(output, f)
+    f.data.input = input
+    f.data.output = output
+    return output
+end
+
+end
+
