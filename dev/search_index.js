bbee26c68342933349e401f1e361e438747e562c
Documenter.jl <documenter@juliadocs.github.io>
2023-11-03 14:09:05 +0000

    build based on 6a7f246

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = JuReto","category":"page"},{"location":"#JuReto","page":"Home","title":"JuReto","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for JuReto.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [JuReto]","category":"page"}]
+}
