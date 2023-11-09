0edef2c1e28de342bcf3f8ac204ad3f491d24b01
takuma I <baru.poke.15@gmail.com>
2023-11-09 17:52:29 +0900

    Step11完了

--- src/JuReto.jl
+++ src/JuReto.jl
@@ -1,9 +1,9 @@
 module JuReto
 
 # Write your package code here.
-export Variable, Square, square, Exp, exp, numerical_diff, backward
-include("step09.jl")
-using .Step09
+export Variable, backward, Square, square, Exp, exp, Add, numerical_diff
+include("step11.jl")
+using .Step11
 
 include("step04.jl")
 using .Step04
---
+++ src/step11.jl
@@ -0,0 +1,131 @@
+module Step11
+abstract type MyFunction end
+using JuReto
+
+export Variable, backward, Square, square, Exp, exp, Add
+
+mutable struct Variable{T<:Union{Real, Array{ElT} where {ElT<:Real}}}
+    data::T
+    grad::Union{T, Nothing}
+    creator::Union{MyFunction, Nothing}
+end
+
+function Variable(data)
+    Variable(data, nothing, nothing)
+end
+
+function set_creator(val::Variable, func::MyFunction)
+    val.creator = func
+end
+
+function backward(val::Variable)
+    if val.grad === nothing
+        initial_grad = collect(val.data) |> similar |> (x->fill!(x, one(eltype(x)))) # 同一形状・データ型の1埋め配列
+        if ndims(initial_grad) == 0
+            val.grad = initial_grad[1] # 0次元配列をスカラーに変更
+        else
+            val.grad = initial_grad
+        end
+    end
+
+    funcs = MyFunction[val.creator]
+    while !isempty(funcs)
+        f = pop!(funcs)
+        x, y = f.data.input, f.data.output
+        x.grad = backward(f, y.grad)
+
+        if x.creator !== nothing
+            push!(funcs, x.creator)
+        end
+    end
+end
+
+mutable struct FunctionData
+    inputs::Union{Vector{Variable}, Nothing}
+    outputs::Union{Vector{Variable}, Nothing}
+end
+
+function FunctionData()
+    FunctionData(nothing, nothing)
+end
+
+function (f::MyFunction)(inputs::Vector{<:Variable})
+    xs = [input.data for input in inputs]
+    ys = forward(f, xs)
+    outputs = Variable.(ys)
+    
+    for output in outputs
+        set_creator(output, f)
+    end
+
+    f.data.inputs = inputs
+    f.data.outputs = outputs
+    return outputs
+end
+
+struct Square <: MyFunction
+    data::FunctionData
+end
+
+function Square()
+    Square(FunctionData())
+end
+
+# 本当はJuliaの演算子に多重ディスパッチでメソッド追加したいが、2乗限定の関数はない
+# pow()関数を実装時に行う
+function square(x::Variable)
+    Square()(x)
+end
+
+function forward(f::Square, x)
+    x^2
+end
+
+function backward(f::Square, dy)
+    x = f.data.input.data
+    2x*dy
+end
+
+struct Exp <: MyFunction
+    data::FunctionData
+end
+
+function Exp()
+    Exp(FunctionData())
+end
+
+import Base: exp
+function exp(x::Variable)
+    Exp()(x)
+end
+
+function forward(f::Exp, x)
+    exp(x)
+end
+
+function backward(f::Exp, dy)
+    x = f.data.input.data
+    exp(x)*dy
+end
+
+struct Add <: MyFunction
+    data::FunctionData
+end
+
+function Add()
+    Add(FunctionData(nothing, nothing))
+end
+
+# import Base; +
+# function +(xs::Vector{Variable})
+#     Add()(xs)
+# end
+
+function forward(f::Add, xs)
+    x0, x1 = xs[1], xs[2]
+    y = x0 + x1
+    return [y]
+end
+
+end
+
