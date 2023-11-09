a7ead03068b85894b6ea95e0187a91f549b8bb19
takuma I <baru.poke.15@gmail.com>
2023-11-09 16:41:46 +0900

    Step10完了

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,25 @@
 using JuReto
 using Test
 
-@testset "JuReto.jl" begin
+@testset "square" begin
     # Write your tests here.
+    x = Variable(2.0)
+    @test square(x).data == 4.0
+    
+    x = Variable(3.0)
+    backward(square(x))
+    @test x.grad == 6.0
+    @test isapprox(x.grad, numerical_diff(Square(), x)) 
+    
+    x = Variable(rand(Float32, 1)[1])
+    backward(square(x))
+    @test isapprox(x.grad, numerical_diff(Square(), x))
+end
+
+@testset "exp" begin
+    x = Variable(2.0)
+    y = exp(x)
+    @test y.data == exp(2.0)
+    backward(y)
+    @test isapprox(x.grad, numerical_diff(Exp(), x)) 
 end
