80f0cc915947073c4231c5110a5fba5e8a3a119e
takuma I <baru.poke.15@gmail.com>
2023-11-24 23:10:20 +0900

    Step20完了

--- src/JuReto.jl
+++ src/JuReto.jl
@@ -5,8 +5,8 @@ export Variable, backward!, init_grad!
 export backward, Square, square, Exp, exp, Add, numerical_diff, +
 export no_grad
 
-include("step19.jl")
-using .Step19
+include("step20.jl")
+using .Step20
 
 include("step04.jl")
 using .Step04
