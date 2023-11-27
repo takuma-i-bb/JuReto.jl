058cc714f516cb6e54e2e27ed8119e2176025a5f
takuma I <baru.poke.15@gmail.com>
2023-11-28 04:09:43 +0900

    多変数関数の数値微分

--- src/step04.jl
+++ src/step04.jl
@@ -2,6 +2,14 @@ module Step04
 using JuReto
 export numerical_diff
 
+function modify_argument(func, arg_index, args)
+    return (new_arg) -> begin
+        modified_args = collect(args)
+        modified_args[arg_index] = new_arg
+        return func(modified_args...)
+    end
+end
+
 function numerical_diff(f, x::Variable, eps=1e-4)
     x0 = Variable(x.data - eps)
     x1 = Variable(x.data + eps)
@@ -9,4 +17,13 @@ function numerical_diff(f, x::Variable, eps=1e-4)
     y1 = f(x1)
     (y1.data - y0.data) / (2eps)
 end
+
+function numerical_diff(f, xs::Vector{T}; eps=1e-4) where {T<:Variable}
+    dxs = Vector{promote_type([typeof(x.data) for x in xs]...)}(undef, length(xs))
+    for i in 1:length(xs)
+        dxs[i] = numerical_diff(modify_argument(f, i, xs), xs[i])
+    end
+    return Tuple(dxs)
+end
+
 end
