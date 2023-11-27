09bc6305f840b6c8718989fc2c49bc87cb0cc709
takuma I <baru.poke.15@gmail.com>
2023-11-28 03:35:21 +0900

    Step22完了

--- src/JuReto.jl
+++ src/JuReto.jl
@@ -2,11 +2,11 @@ module JuReto
 
 # Write your package code here.
 export Variable, backward!, init_grad!
-export backward, Square, square, Exp, exp, Add, numerical_diff, +
+export backward, Square, square, Exp, exp, Add, numerical_diff, +, *
 export no_grad
 
-include("step21.jl")
-using .Step21
+include("step22.jl")
+using .Step22
 
 include("step04.jl")
 using .Step04
